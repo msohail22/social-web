5ec48eea14c20ab0703046060631beeba529daf3
GANDHARVA7REDDY <karrigandharvreddy@gmail.com>
2026-05-03 23:18:01 +0530

    feat: add NotFound component and route for 404 handling

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,11 +2,13 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
